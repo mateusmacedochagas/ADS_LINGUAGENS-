5f6a46f259fdcc677aacffec4e80cfbf4b6cacca
mateusmacedochagas <mateusmacedochagas@gmail.com>
2026-08-10 21:56:32 -0300

    Create AULA 2.c

---
+++ AULA 2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main () {
+int a = 10;
+int b  = 20;
+int c = 30 ;
+int d = (a+b+c)/3;
+
+printf("A média de %d, %d, %d = %d , a, b, c, d);
+
+  return 0;
+
+}
+
+  
